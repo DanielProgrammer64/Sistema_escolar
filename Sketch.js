8b01ed129dd9d09933afc97030f8a0eb47c8a03d
DanielProgrammer64 <danielinternet567@gmail.com>
2024-04-02 14:32:46 -0300

    Informações do usuário, pequeno Formulário e estilo simples e funcional desenvolvidos!

--- Sketch.js
+++ Sketch.js
@@ -1,2 +1,21 @@
-console.info(window.innerHeight, window.innerWidth)
+let infos = {
+  nome: "Daniel Sansão",
+  idade: 16,
+  peso: 71.3,
+  altura_metros: 1.77,
+};
 
+let Curso = prompt("Insira aqui o curso");
+
+let Resultado = document.getElementById("resultado");
+
+let texto = `o nome do aluno é ${infos.nome}
+ sua idade é ${infos.idade} 
+ seu peso é ${infos.peso} 
+ sua altura é de ${infos.altura_metros} Metros 
+ e seu curso é ${Curso} `;
+
+Resultado.innerText = texto;
+
+console.info(window.innerHeight, window.innerWidth);
+alert("esta página está em funcionamento");
