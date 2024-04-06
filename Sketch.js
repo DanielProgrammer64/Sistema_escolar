3440782ec3dac5d4cfb91ed91af6177468d6adb7
DanielProgrammer64 <danielinternet567@gmail.com>
2024-04-06 15:35:44 -0300

    Mais uma organização básica, além de um sistema de preenchimento e nome de uma turma de exemplo

--- Sketch.js
+++ Sketch.js
@@ -7,15 +7,45 @@ let infos = {
 
 let Curso = prompt("Insira aqui o curso");
 
-let Resultado = document.getElementById("resultado");
+let Resultado1 = document.getElementById("dados_aluno");
 
-let texto = `o nome do aluno é ${infos.nome}
+let texto = `o nome do aluno especificado é ${infos.nome}
  sua idade é ${infos.idade} 
  seu peso é ${infos.peso} 
  sua altura é de ${infos.altura_metros} Metros 
  e seu curso é ${Curso} `;
 
-Resultado.innerText = texto;
+Resultado1.innerText = texto;
+
+// array de notas
+
+let Notas = [Number, Number, Number, Number];
+
+Notas[0] = prompt("insira aqui a nota 1");
+Notas[1] = prompt("insira aqui a nota 2");
+Notas[2] = prompt("insira aqui a nota 3");
+Notas[3] = prompt("insira aqui a nota 4");
+
+console.info(Notas);
+
+// nome dos alunos
+
+let Nome_dos_alunos = [
+  "Daniele",
+  "Felipe",
+  "Júlio",
+  "Franco",
+  "Marcos",
+  "Júlia",
+  "Sansão",
+  "Maicon",
+];
+
+let turma = Nome_dos_alunos.join(', ');
+
+let Resultado2 = document.getElementById("turma");
+let texto2 = `A turma é composta pelos alunos: ${turma}`;
+Resultado2.innerText = texto2;
 
 console.info(window.innerHeight, window.innerWidth);
 alert("esta página está em funcionamento");
